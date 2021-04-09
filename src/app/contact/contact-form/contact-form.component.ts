7a1842022007926f1b672ace1696598f09901d99
Christoph Tillmann <christoph.tillmann@basf.com>
2021-04-09 14:41:23 +0200

    Template driven form implemented

--- src/app/contact/contact-form/contact-form.component.ts
+++ src/app/contact/contact-form/contact-form.component.ts
@@ -1,13 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.scss']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild('myForm', { 'read': NgForm })
+  myForm: NgForm | undefined;
 
   constructor() { }
+
+  ngAfterViewInit(): void {
+    this.myForm?.value;
+  }
 
   ngOnInit(): void {
   }
@@ -16,6 +25,14 @@ export class ContactFormComponent implements OnInit {
 
   startEmail = "1234@email.de"
 
+  street: string = "";
+
   searchstring = "";
+
+  send(formData: any): void {
+    console.log("Data from Form", formData);
+    console.log("##########################")
+    console.log("Data from TS class", this.myForm?.value);
+  }
 
 }
